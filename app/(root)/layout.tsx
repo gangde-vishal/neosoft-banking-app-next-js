ecb4cc40d1dbcf3c74a45148bb107a83ec61b7fc
Vishal Gangde <gangdesonu6@gmail.com>
2024-06-24 19:28:23 +0530

    Work on Home page that includes(Counter, Chart, Header and Total Balance) and Sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedInd = { firstName: "Vishal", lastName: "Gangde" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex w-full h-screen font-inter">
+      <Sidebar user={loggedInd} />
       {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Vishal" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and translations efficiently"
+            user={loggedIn?.firstName || "Guest"}
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1246.11}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
